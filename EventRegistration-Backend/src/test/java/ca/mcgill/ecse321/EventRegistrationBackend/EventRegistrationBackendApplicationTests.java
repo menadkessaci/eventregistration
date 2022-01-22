4b641af6b9e8d7ab9837f7431ec6787a496083f0
menadkessaci <menad.kessaci@mail.mcgill.ca>
2022-01-21 19:20:07 -0500

    Uncommenting @SpringBootTest in test file

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
@@ -3,9 +3,8 @@ package ca.mcgill.ecse321.EventRegistrationBackend;
 /*
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 */
+@SpringBootTest
 class EventRegistrationBackendApplicationTests {
 
 	@Test
