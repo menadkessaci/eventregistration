e0760e1be68e2fc3a5a6068eb1f523fbaaa707f2
menadkessaci <menad.kessaci@mail.mcgill.ca>
2022-01-21 21:06:31 -0500

    Back to tests commented - building version on git

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
@@ -1,13 +1,13 @@
 package ca.mcgill.ecse321.EventRegistrationBackend;
 
-
+/*
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest*/
 class EventRegistrationBackendApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
