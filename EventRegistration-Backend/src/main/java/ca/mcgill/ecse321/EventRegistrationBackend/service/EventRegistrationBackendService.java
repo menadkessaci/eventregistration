37b9928f6ee3dbb461c7a0b194e1ccdb0c949389
menadkessaci <menad.kessaci@mail.mcgill.ca>
2022-02-17 23:33:38 -0500

    Ready for 2.7 (Code Coverage)

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/EventRegistrationBackend/service/EventRegistrationBackendService.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/EventRegistrationBackend/service/EventRegistrationBackendService.java
@@ -28,16 +28,108 @@ public class EventRegistrationBackendService {
 
 	@Transactional
 	public Person createPerson(String name) {
+		if (name == null || name.trim().length() == 0) {
+			throw new IllegalArgumentException("Person name cannot be empty!");
+		}
 		Person person = new Person();
 		person.setName(name);
 		personRepository.save(person);
 		return person;
 	}
-
+	
 	@Transactional
 	public Person getPerson(String name) {
-		Person person = personRepository.findPersonByName(name);
-		return person;
+	    if (name == null || name.trim().length() == 0) {
+	        throw new IllegalArgumentException("Person name cannot be empty!");
+	    }
+	    Person person = personRepository.findPersonByName(name);
+	    return person;
+	}
+
+	@Transactional
+	public Event getEvent(String name) {
+	    if (name == null || name.trim().length() == 0) {
+	        throw new IllegalArgumentException("Event name cannot be empty!");
+	    }
+	    Event event = eventRepository.findEventByName(name);
+	    return event;
+	}
+
+	@Transactional
+	public Event createEvent(String name, Date date, Time startTime, Time endTime) {
+	    // Input validation
+	    String error = "";
+	    if (name == null || name.trim().length() == 0) {
+	        error = error + "Event name cannot be empty! ";
+	    }
+	    if (date == null) {
+	        error = error + "Event date cannot be empty! ";
+	    }
+	    if (startTime == null) {
+	        error = error + "Event start time cannot be empty! ";
+	    }
+	    if (endTime == null) {
+	        error = error + "Event end time cannot be empty! ";
+	    }
+	    if (endTime != null && startTime != null && endTime.before(startTime)) {
+	        error = error + "Event end time cannot be before event start time!";
+	    }
+	    error = error.trim();
+	    if (error.length() > 0) {
+	        throw new IllegalArgumentException(error);
+	    }
+
+	    Event event = new Event();
+	    event.setName(name);
+	    event.setDate(date);
+	    event.setStartTime(startTime);
+	    event.setEndTime(endTime);
+	    eventRepository.save(event);
+	    return event;
+	}
+
+	public Registration register(Person person, Event event) {
+	    String error = "";
+	    if (person == null) {
+	        error = error + "Person needs to be selected for registration! ";
+	    } else if (!personRepository.existsById(person.getName())) {
+	        error = error + "Person does not exist! ";
+	    }
+	    if (event == null) {
+	        error = error + "Event needs to be selected for registration!";
+	    } else if (!eventRepository.existsById(event.getName())) {
+	        error = error + "Event does not exist!";
+	    }
+	    if (registrationRepository.existsByPersonAndEvent(person, event)) {
+	        error = error + "Person is already registered to this event!";
+	    }
+	    error = error.trim();
+
+	    if (error.length() > 0) {
+	        throw new IllegalArgumentException(error);
+	    }
+
+	    Registration registration = new Registration();
+		registration.setId(person.getName().hashCode() * event.getName().hashCode());
+	    registration.setPerson(person);
+	    registration.setEvent(event);
+
+	    registrationRepository.save(registration);
+
+	    return registration;
+	}
+
+
+	@Transactional
+	public List<Event> getEventsAttendedByPerson(Person person) {
+	    if (person == null ) {
+	        throw new IllegalArgumentException("Person cannot be null!");
+	    }
+	    List<Event> eventsAttendedByPerson = new ArrayList<>();
+	    for (Registration r : registrationRepository.findByPerson(person)) {
+	        eventsAttendedByPerson.add(r.getEvent());
+	    }
+	    return eventsAttendedByPerson;
 	}
 
 	@Transactional
@@ -45,52 +137,18 @@ public class EventRegistrationBackendService {
 		return toList(personRepository.findAll());
 	}
 
-	@Transactional
-	public Event createEvent(String name, Date date, Time startTime, Time endTime) {
-		Event event = new Event();
-		event.setName(name);
-		event.setDate(date);
-		event.setStartTime(startTime);
-		event.setEndTime(endTime);
-		eventRepository.save(event);
-		return event;
-	}
-
-	@Transactional
-	public Event getEvent(String name) {
-		Event event = eventRepository.findEventByName(name);
-		return event;
-	}
+	
 
 	@Transactional
 	public List<Event> getAllEvents() {
 		return toList(eventRepository.findAll());
 	}
 
-	@Transactional
-	public Registration register(Person person, Event event) {
-		Registration registration = new Registration();
-		registration.setId(person.getName().hashCode() * event.getName().hashCode());
-		registration.setPerson(person);
-		registration.setEvent(event);
-
-		registrationRepository.save(registration);
-
-		return registration;
-	}
+	
 
 	@Transactional
 	public List<Registration> getAllRegistrations(){
 		return toList(registrationRepository.findAll());
-	}
-
-	@Transactional
-	public List<Event> getEventsAttendedByPerson(Person person) {
-		List<Event> eventsAttendedByPerson = new ArrayList<>();
-		for (Registration r : registrationRepository.findByPerson(person)) {
-			eventsAttendedByPerson.add(r.getEvent());
-		}
-		return eventsAttendedByPerson;
 	}
 
 	private <T> List<T> toList(Iterable<T> iterable){
